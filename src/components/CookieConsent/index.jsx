a37c48d9c0c47a9f1afdedfc2c66147eb9eb8f29
yellowCrimsonGator <112893988+yellowCrimsonGator@users.noreply.github.com>
2024-01-31 01:52:56 +0800

    fix: simple analytics (#98)

--- src/components/CookieConsent/index.jsx
+++ src/components/CookieConsent/index.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import { useCookies } from "react-cookie";
 import CookieBanner from "./CookieBanner";
+import Head from '@docusaurus/Head';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 const COOKIES_CONSENT_NAME = "cookie-consent-given";
 const COOKIES_CONSENT_VALUE = 1;
 
 export default function CookiesConsent() {
-  if (typeof window === "undefined") return null;
+  const isBrowser = useIsBrowser();
+  if(!isBrowser) return null
   const [cookies, setCookie] = useCookies();
   const [consentGiven, setIsConsentGiven] = React.useState(
     cookies?.[COOKIES_CONSENT_NAME] === COOKIES_CONSENT_VALUE
   );
 
   return consentGiven ? (
-    <>
+    <Head>
       <script
         async
         defer
         src="https://scripts.simpleanalyticscdn.com/latest.js"
       ></script>
-      <noscript>
-        <img
-          src="https://queue.simpleanalyticscdn.com/noscript.gif"
-          alt=""
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-      </noscript>
-    </>
+      <noscript>{`<img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerPolicy="no-referrer-when-downgrade" />`}</noscript>
+    </Head>
   ) : (
     <CookieBanner
       onClick={() => {
